dc9e17410ede9b9ccff330fb911d260537c738c3
Humberto Jr <halencarjunior@gmail.com>
2019-11-28 14:33:49 -0300

    Version 0.0.2 - Bug fix

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # IPyCrawler
 
 Simple Script for Lookup hostnames in IP Ranges.
+
 This tool is based in the Old and Super Useful ipcrawl.
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)
 
 ## Requirements:
 
@@ -18,12 +21,14 @@ optional arguments:
 
   -h, --help            show this help message and exit
   
-  -s [STARTIP], --startip [STARTIP] Start IP
+  -s [STARTIP], --startip [STARTIP] Start IP (This argument is required)
   
-  -e [ENDIP], --endip [ENDIP] End IP
+  -e [ENDIP], --endip [ENDIP] End IP (Optional argument. For single lookup use only -s. -o options is not allowed for single lookup)
   
   -o [OUTPUT], --output [OUTPUT] Output results to a file e.g results.txt
   
   --version             show program's version number and exit
+
+## Screenshots
 
 ![IPyCrawler](https://imgur.com/N2tHobe.jpg)
--- ipycrawl.py
+++ ipycrawl.py
@@ -2,17 +2,21 @@ import socket
 import ipaddress
 import argparse
 
-VERSION="0.0.1"
-MESSAGE="IPyCrawl Version 0.0.1\n\n"
+VERSION="0.0.2"
+MESSAGE="IPyCrawl Version 0.0.2\n\n"
 
 def save_file(name_file, info):
-     f=open(name_file,"a+")
-     f.write(info+"\n")
-     f.close()
+    try:
+        f=open(name_file,"a+")
+        f.write(info+"\n")
+        f.close()
+    except OSError as err:
+        print("OS error: {0}".format(err))
 
 def lookup_host(start_ip,end_ip,name_file=None,output=False):
+    print("[.] Starting Check in range of hosts from {} to {} \n" .format(ipaddress.IPv4Address(start_ip),ipaddress.IPv4Address(end_ip)))
+
     for ip_int in range(int(start_ip), int(end_ip)+1):
-        #print(ipaddress.IPv4Address(ip_int))
         try:
             hostName = socket.gethostbyaddr(str(ip_int))
             strResult = "[+] Host Name for the IP address {} is {}".format(ipaddress.IPv4Address(ip_int), hostName)
@@ -24,6 +28,18 @@ def lookup_host(start_ip,end_ip,name_file=None,output=False):
             print(strResultError)
             if(output==True):
                 save_file(name_file,strResultError)
+    print("\n[x] Process finished.")
+
+def lookup_single_host(start_ip):
+    print("[.] Starting Check in a single host\n")
+    try:
+        hostName = socket.gethostbyaddr(str(start_ip))
+        strResult = "[+] Host Name for the IP address {} is {}".format(ipaddress.IPv4Address(start_ip), hostName)
+        print(strResult)
+    except OSError:
+        strResultError = "[-] Error trying to lookup host: %s" % ipaddress.IPv4Address(start_ip)
+        print(strResultError)
+    print("\n[x] Process finished.")
 
 def main():
     parser = argparse.ArgumentParser(prog='IPyCrawler', formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -37,21 +53,24 @@ ___________      _____                    _
              __/ |                                     
             |___/                                    
 
-Version 0.0.1 
+Version 0.0.2 
 Script for Lookup Hostname from Hosts
 
 ''')
     parser.add_argument('-s', '--startip',  nargs='?', required=True, help='Start IP')
     parser.add_argument('-e', '--endip', nargs='?', help='End IP')
     parser.add_argument('-o', '--output',  nargs='?', help='Output results to a file e.g results.txt')
-    parser.add_argument('--version', action='version', version='%(prog)s 0.0.1')
+    parser.add_argument('--version', action='version', version='%(prog)s 0.0.2')
     args = parser.parse_args()
 
     if (args.startip and args.endip and args.output):
         start_ip = ipaddress.IPv4Address(args.startip)
         end_ip = ipaddress.IPv4Address(args.endip)
         name_file = args.output
-        f=open(name_file,"w+")
+        try:
+            f=open(name_file,"w+")
+        except OSError as err:
+            print("OS error: {0}".format(err))
         
         print(parser.description)
         lookup_host(start_ip,end_ip,name_file,True)
@@ -62,6 +81,10 @@ Script for Lookup Hostname from Hosts
         print(parser.description)
         lookup_host(start_ip,end_ip,None,False)
 
+    if (args.startip and not args.endip and not args.output):
+        start_ip = ipaddress.IPv4Address(args.startip)
+        print(parser.description)
+        lookup_single_host(start_ip)
 
 if __name__ == '__main__':
     main()
