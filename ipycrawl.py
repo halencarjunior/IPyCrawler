0716a16fb43c8138e8b80fa16b1cd1f846331ee8
Humberto Jr <halencarjunior@gmail.com>
2019-11-28 17:44:45 -0300

    Added reverse DNS lookup

--- ipycrawl.py
+++ ipycrawl.py
@@ -2,19 +2,20 @@ import socket
 import ipaddress
 import argparse
 import csv
+from dns import reversename, resolver
 
 VERSION="0.0.2"
 MESSAGE="IPyCrawl Version 0.0.2\n\n"
 
-def save_file_csv(name_file, info):
+def save_file_csv(name_file, info, reversed):
     domain=str(info[0])
-    reverse=str(info[1]).strip("[']")
+    #reverse=str(info[1]).strip("[']")
     hostip=str(info[2]).strip("[']")
 
     with open(name_file, mode='a+') as host_csv:
         hosts = csv.writer(host_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-        hosts.writerow([domain, reverse, hostip ])
+        hosts.writerow([domain, reversed, hostip ])
 
 def save_file(name_file, info):
     try:
@@ -30,18 +31,22 @@ def lookup_host(start_ip,end_ip,name_file=None,output=False,csv=False):
         print("[i] Exporting in csv format\n")
     else:
         print("[i] Exporting in normal format\n")
+    
     for ip_int in range(int(start_ip), int(end_ip)+1):
         try:
             hostName = socket.gethostbyaddr(str(ip_int))
-            hostNameStr = str(hostName[0]) + " Reverse: " + str(hostName[1])
+            #hostNameStr = str(hostName[0]) + " Reverse: " + str(hostName[1])
+            currentHostIp=ipaddress.IPv4Address(ip_int)
+            rev_name = reversename.from_address(str(currentHostIp))
+            reversed_dns = str(resolver.query(rev_name,"PTR")[0])
 
-            strResult = "[+] Host Name for the IP address {} is {}".format(ipaddress.IPv4Address(ip_int), hostNameStr)
+            strResult = "[+] Host Name for the IP address {} is {} - Reverse DNS: {}".format(ipaddress.IPv4Address(ip_int), hostName[0], reversed_dns)
             print(strResult)
             if(output==True):
                 if(csv==False):
                     save_file(name_file,strResult)
                 else:
-                    save_file_csv(name_file,hostName)
+                    save_file_csv(name_file,hostName,reversed_dns)
 
         except OSError:
             strResultError = "[-] Error trying to lookup host: %s" % ipaddress.IPv4Address(ip_int)
@@ -55,7 +60,11 @@ def lookup_single_host(start_ip):
     print("[.] Starting Check in a single host\n")
     try:
         hostName = socket.gethostbyaddr(str(start_ip))
-        strResult = "[+] Host Name for the IP address {} is {}".format(ipaddress.IPv4Address(start_ip), hostName)
+        currentHostIp=ipaddress.IPv4Address(start_ip)
+        rev_name = reversename.from_address(str(currentHostIp))
+        reversed_dns = str(resolver.query(rev_name,"PTR")[0])
+
+        strResult = "[+] Host Name for the IP address {} is {} - Reverse DNS: {}".format(ipaddress.IPv4Address(start_ip), hostName[0], reversed_dns)
         print(strResult)
     except OSError:
         strResultError = "[-] Error trying to lookup host: %s" % ipaddress.IPv4Address(start_ip)
@@ -126,5 +135,3 @@ Script for Lookup Hostname from Hosts
 
 if __name__ == '__main__':
     main()
-
-
